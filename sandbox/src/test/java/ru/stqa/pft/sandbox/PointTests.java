bee4dc2133e1c2e2ddc42937731288f07ab49a0f
tpanina <tpanina@issart.com>
2020-07-06 15:47:31 +0600

    PointsTests is edited

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -20,4 +20,11 @@ public class PointTests {
         Point p2 = new Point(2,3);
         Assert.assertEquals(distance(p1, p2), 1.4142135623730951);
     }
+
+    @Test
+    public void testDistanceIsNotNull() {
+        Point p1 = new Point(1,2);
+        Point p2 = new Point(2,3);
+        Assert.assertNotNull(p1.distance(p2));
+    }
 }
