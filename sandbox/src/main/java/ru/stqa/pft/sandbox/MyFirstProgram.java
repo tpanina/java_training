92ddda9ec0effe7fd658db51c67f0d9f888e010c
tpanina <tpanina@issart.com>
2020-07-06 15:33:59 +0600

    Points files were edited

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,9 +13,17 @@ public class MyFirstProgram {
         Rectangle r = new Rectangle(4,6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 
+        //функция
         Point p1 = new Point(1,2);
         Point p2 = new Point(2,3);
+        System.out.println("Расстояние между точкой p1 с координатами x = " + p1.x + " и y = " + p1.y + " и точкой p2 с координатами x = " + p2.x + " и y = " + p2.y + " равно " + distance(p1,p2));
+
+        //метод
         System.out.println("Расстояние между точкой p1 с координатами x = " + p1.x + " и y = " + p1.y + " и точкой p2 с координатами x = " + p2.x + " и y = " + p2.y + " равно " + p1.distance(p2));
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x));
     }
 
     public static void hello(String somebody) {
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -3,12 +3,21 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static ru.stqa.pft.sandbox.MyFirstProgram.distance;
+
 public class PointTests {
 
     @Test
-    public void testDistance() {
+    public void testDistanceMethod() {
         Point p1 = new Point(1,2);
         Point p2 = new Point(2,3);
-        Assert.assertEquals(p1.distance(p1,p2), 1.4142135623730951);
+        Assert.assertEquals(p1.distance(p2), 1.4142135623730951);
+    }
+
+    @Test
+    public void testDistanceFunction() {
+        Point p1 = new Point(1,2);
+        Point p2 = new Point(2,3);
+        Assert.assertEquals(distance(p1, p2), 1.4142135623730951);
     }
 }
