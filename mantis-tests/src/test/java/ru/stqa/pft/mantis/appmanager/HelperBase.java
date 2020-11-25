d32aa9eaa0fa569bb7d3f732a97272a335f601db
tpanina <tpanina@issart.com>
2020-11-25 15:39:26 +0600

    Registration Tests with integrated Mail Server

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HelperBase.java
@@ -0,0 +1,54 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.*;
+
+import java.io.File;
+
+public class HelperBase {
+    protected WebDriver driver;
+    protected ApplicationManager app;
+
+    public HelperBase(ApplicationManager app) {
+        this.app = app;
+        this.driver = app.getDriver();
+    }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        if (text != null) {
+            String existingText = driver.findElement(locator).getAttribute("value");
+            if (! text.equals(existingText)) {
+                driver.findElement(locator).clear();
+                driver.findElement(locator).sendKeys(text);
+            }
+        }
+    }
+
+    protected void attach(By locator, File file) {
+        if (file != null) {
+                driver.findElement(locator).sendKeys(file.getAbsolutePath());
+            }
+        }
+
+    protected void isAlertPresent(WebDriver driver) {
+        try {
+            Alert alert = driver.switchTo().alert();
+            alert.accept();
+        } catch (NoAlertPresentException e) {
+            return;
+        }
+    }
+
+    protected void click(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
+    }
+}
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,17 +1,24 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
-public class RegistrationHelper {
-    private final ApplicationManager app;
-    private WebDriver driver;
+public class RegistrationHelper extends HelperBase {
 
     public RegistrationHelper(ApplicationManager app) {
-        this.app = app;
-        driver = app.getDriver();
+        super (app);
     }
 
     public void start(String username, String email) {
         driver.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+        type(By.name("username"), username);
+        type(By.name("email"), email);
+        click(By.cssSelector("input[value='Signup']"));
+    }
+
+    public void finish(String confirmationLink, String password) {
+        driver.get(confirmationLink);
+        type(By.name("password"), password);
+        type(By.name("password_confirm"), password);
+        click(By.cssSelector("input[value='Update User']"));
     }
 }
